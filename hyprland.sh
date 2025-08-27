f9e3538e8e66439bbb985296d2983716e368ea7b
Papa Ours <134229865+PapaOursPolaire@users.noreply.github.com>
2025-08-27 14:40:47 +0200

    Update hyprland.sh

--- hyprland.sh
+++ hyprland.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Script d'installation d'Hyprland compatible sur plusieurs distros Linux
-# Version 236.7 - 27/08/2025 14:26 : Mise à jour corrigée avec détection GPU/CPU et améliorations
+# Version 236.7 - 27/08/2025 14:40 : Mise à jour corrigée avec détection GPU/CPU et améliorations
 # Compatible: Arch, Ubuntu/Debian, Fedora, OpenSUSE
 
 set -e
