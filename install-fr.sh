90c36447c70d4d41bb87025be2ed5303dba1359a
Papa Ours <134229865+PapaOursPolaire@users.noreply.github.com>
2025-09-10 21:13:19 +0200

    Update install-fr.sh

--- install-fr.sh
+++ install-fr.sh
@@ -11,7 +11,7 @@ fi
 # Script d'installation automatisée Arch Linux
 # Made by PapaOursPolaire - available on GitHub
 # Version: 534.5, correctif 5 de la version 534.5
-# Mise à jour : 02/09/2025 à 22:24
+# Mise à jour : 10/09/2025 à 21:13
 # PRENDRE  LA  NOUVELLE VERSION après un dos2unix SUR LINUX ou dans le chroot, pacman -Sy dos2unix
 # Correction de 2358 erreurs référencées par ShellCheck et par la conssole  TTY de l'ISO corrigées
 # Erreurs à l'étape 17  : ne paas installer paru dans le temp
